909336d95e6b9ce1dc7fa21b2124643699c1a532
Helcio da Silva <helciodasilva@gmail.com>
2018-02-19 07:46:25 -0300

    Capítulo 6

--- tests/CDC/Loja/Carrinho/CarrinhoDeComprasTest.php
+++ tests/CDC/Loja/Carrinho/CarrinhoDeComprasTest.php
@@ -4,39 +4,75 @@ namespace CDC\Loja\Carrinho;
 
 use CDC\Loja\Test\TestCase,
         CDC\Loja\Carrinho\CarrinhoDeCompras,
-        CDC\Loja\Produto\Produto;
+        CDC\Loja\Produto\Produto,
+        CDC\Loja\Test\Builder\CarrinhoDeComprasBuilder;
+        
 
 class CarrinhoDeComprasTest extends TestCase
 {
+
+    private $carrinho;
+    
+    protected function setUp()
+    {
+        $this->carrinho = new CarrinhoDeCompras();
+        parent::setUp();
+    }
+
+
     public function testDeveRetornarZeroSeCarrinhoVazio()
     {
-        $carrinho = new CarrinhoDeCompras();
-
-        $valor = $carrinho->maiorValor();
+        $valor = $this->carrinho->maiorValor();
 
         $this->assertEquals(0, $valor, null, 0.0001);
     }
 
     public function testDeveRetornarValorDoItemSeCarrinhoCom1Elemento()
     {
-        $carrinho = new CarrinhoDeCompras();
-        $carrinho->adiciona(new Produto("Geladeira", 900.00, 1));
+        $this->carrinho->adiciona(new Produto("Geladeira", 900.00, 1));
 
-        $valor = $carrinho->maiorValor();
+        $valor = $this->carrinho->maiorValor();
 
         $this->assertEquals(900.00, $valor, null, 0.0001);
     }
 
     public function testDeveRetornarMaiorValorSeCarrinhoComMuitosElementos()
     {
-        $carrinho = new CarrinhoDeCompras();
-        $carrinho->adiciona(new Produto("Geladeira", 900.00, 1));
-        $carrinho->adiciona(new Produto("Fogão", 1500.00, 1));
-        $carrinho->adiciona(new Produto("Máquina de lavar", 750.00, 1));
+        $this->carrinho->adiciona(new Produto("Geladeira", 900.00, 1));
+        $this->carrinho->adiciona(new Produto("Fogão", 1500.00, 1));
+        $this->carrinho->adiciona(new Produto("Máquina de lavar", 750.00, 1));
 
-        $valor = $carrinho->maiorValor();
+        $valor = $this->carrinho->maiorValor();
 
         $this->assertEquals(1500.00, $valor, null, 0.0001);
+    }
+
+    public function testParaMostrarOUsoDoTestDataBuilder()
+    {
+        $carrinho = (new CarrinhoDeComprasBuilder())->comItens(300.0, 700.0, 200.0, 500.0)->cria();
+
+        $maiorValor = $carrinho->maiorValor();
+        $this->assertEquals(700.0, $maiorValor, null, 0.0001);
+    }
+
+    public function testDeveAdicionarItens()
+    {
+        $this->assertEmpty($this->carrinho->getProdutos());
+        $produto = new Produto("Geladeira", 900.0, 1);
+        $this->carrinho->adiciona($produto);
+        $esperado = count($this->carrinho->getProdutos());
+        $this->assertEquals(1, $esperado);
+        $this->assertEquals($produto, $this->carrinho->getProdutos()[0]);
+    }
+
+    public function testListaDeProdutos()
+    {
+        $lista = (new CarrinhoDeCompras())
+                ->adiciona(new Produto("Jogo de jantar", 200.00, 1))
+                ->adiciona(new Produto("Jogo de pratos", 100.00, 1));
+        $this->assertEquals(2, count($lista->getProdutos()));
+        $this->assertEquals(200.0, $lista->getProdutos()[0]->getValorUnitario());
+        $this->assertEquals(100.0, $lista->getProdutos()[1]->getValorUnitario());
     }
 
 }
